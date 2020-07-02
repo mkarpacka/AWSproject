461c8a361d7f45d98ee612b8c97feb66be49e802
mkarpacka <209348@edu.p.lodz.pl>
2020-07-02 22:07:56 +0200

    init

---
+++ src/main/java/com/project/aws/Image.java
@@ -0,0 +1,28 @@
+package com.project.aws;
+
+public class Image {
+
+
+    public Image() {}
+
+    public Image(String name) {
+        this.name = name;
+    }
+
+    public Image(int id, String name ) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    private int id;
+
+}
