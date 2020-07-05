5a02b650423e481cb9e6bd2f29fc09891246e1f3
mkarpacka <209348@edu.p.lodz.pl>
2020-07-05 17:55:12 +0200

    little cleanup

--- src/main/java/com/project/aws/S3Config.java
+++ src/main/java/com/project/aws/S3Config.java
@@ -1,7 +1,6 @@
 package com.project.aws;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
--- src/main/java/com/project/aws/S3Services.java
+++ src/main/java/com/project/aws/S3Services.java
@@ -9,13 +9,7 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
